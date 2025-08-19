6ba93040c60b8f0e734c5255d0972be0b5b89bc7
Leticia Mangieri <mangierileticia@gmail.com>
2025-08-19 11:01:23 -0300

    Se cargan los datos

--- js/script.js
+++ js/script.js
@@ -19,3 +19,14 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+function cargarDatos(){
+  fetch("/json/data.json")
+  .then(response =>response.json()) //indico el formato de los datos obtenidos
+  .then(data =>{
+    showData(data.students);    //le paso a la función showData() los nombres y apellidos accediendo a students de la respuesta obtenida
+  })
+  .catch(error =>{
+    console.error('ERROR',error)  //si hay un error lo imprime en la consola
+  });
+}
+cargarDatos();
