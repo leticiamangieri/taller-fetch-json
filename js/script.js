23a5ea73c03c461b1e25851e9b336d11f710ca91
leticiamangieri <mangierileticia@gmail.com>
2025-08-19 11:29:50 -0300

    Cambio en la ruta del archivo json

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function showData(dataArray) {
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
 function cargarDatos(){
-  fetch("/json/data.json")
+  fetch("json/data.json")
   .then(response =>response.json()) //indico el formato de los datos obtenidos
   .then(data =>{
     showData(data.students);    //le paso a la función showData() los nombres y apellidos accediendo a students de la respuesta obtenida
